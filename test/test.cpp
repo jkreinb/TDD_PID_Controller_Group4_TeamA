90d4a182a2a5e29f90252b21ab616cb134a44495
Jon Kreinbrink <jkreinb@gmail.com>
2021-10-02 15:53:51 -0400

    added tests for setting kvalue and getting kvalue

--- test/test.cpp
+++ test/test.cpp
@@ -7,7 +7,6 @@ ControllerPID Test_PID_class;
  * 
  */
 TEST(compute_initialPID, should_pass) {
-
    EXPECT_NEAR((Test_PID_class.ComputePID(50, 20)), 10.650, .2); 
 }
 
@@ -31,4 +30,23 @@ TEST(PID_Converges, should_pass) {
 
   // Tests if current speed is within 1% of set point after 1000 time steps
   EXPECT_NEAR(test_speed, setpoint, .5);
+}
+
+/**
+ * @brief Test to verify set_Kvalues and getters for K values are updated properly
+ * 
+ */
+TEST(KValues_Updated, should_pass) {
+
+  // Sets some K values to ensure set_Kvalues function works
+  Test_PID_class.set_Kvalues(.25,.50,.35);
+
+  // Checks to ensure Kvalues are updated within the class, using the getter functions
+  float retrieved_Kp = Test_PID_class.get_Kp();
+  float retrieved_Ki = Test_PID_class.get_Ki();
+  float retrieved_Kd = Test_PID_class.get_Kd();
+
+  EXPECT_FLOAT_EQ(retrieved_Kp,.25);
+  EXPECT_FLOAT_EQ(retrieved_Ki,.50);
+  EXPECT_FLOAT_EQ(retrieved_Kd,.35);
 }
